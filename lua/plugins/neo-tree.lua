14f9e309f3e765cea40576314b634f0541ab1e77
kebiBGit <elfakkebibofficial@gmail.com>
2025-10-09 21:11:26 +0545

    Neovim config using Lazyvim

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>ff", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>fg", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<C-m>", ":Neotree filesystem close left<CR>",{})
+	end,
+}
