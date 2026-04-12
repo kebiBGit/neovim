f2a5b51f5e324f107ef55072f9b5baa5844a102f
kebiBGit <173374658+kebiBGit@users.noreply.github.com>
2026-04-12 20:13:05 +0545

    startup time

--- init.lua
+++ init.lua
@@ -1,4 +1,16 @@
+local t1 = vim.uv.hrtime()
+
+vim.loader.enable()
+
 require('options')
 require('theme')
 require('plugins')
+
+vim.api.nvim_create_autocmd("UIEnter", {
+	once = true,
+	callback = function()
+		vim.g.startup_time = (vim.uv.hrtime() - t1) / 1e6
+	end,
+})
+
 
