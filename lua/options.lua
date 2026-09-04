34922486f7331334a874e23e878bb0845d3a4f78
kebiBGit <173374658+kebiBGit@users.noreply.github.com>
2026-09-04 23:04:56 +0545

    removed tmux navigator  used simple keymaps instead of plugin

--- lua/options.lua
+++ lua/options.lua
@@ -1,24 +1,25 @@
 vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.tabstop=2 -- Number of spaces that a <Tab> in the file counts for
-vim.opt.softtabstop=2
-vim.opt.shiftwidth=2
+vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.cursorline = true
 vim.g.mapleader = " "
 vim.g.background = "dark"
 
-vim.opt.swapfile = true
+vim.opt.swapfile = false
 
--- Navigate vim panes better
---vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
---vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
---vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
---vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+-- vim splits/panes navigation
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { silent = true })
 
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.opt.scrolloff=10
+vim.opt.scrolloff = 10
 vim.opt.incsearch = true
-vim.opt.hlsearch=true
+vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.o.autoindent = true
@@ -26,16 +27,16 @@ vim.opt.termguicolors = true
 vim.opt.linebreak = true
 vim.opt.signcolumn = "yes:1"
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ timeout = 300 })  -- highlight for 200ms
-  end,
+	callback = function()
+		vim.highlight.on_yank({ timeout = 300 }) -- highlight for 200ms
+	end,
 })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set("n", "<leader>p", '"+p', { noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true })
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 vim.keymap.set("n", "<leader>m", ":tabp<CR>")
 vim.keymap.set("n", "<leader>n", ":tabn<CR>")
 vim.keymap.set("n", "<leader>tab", ":tabnew<CR>")
